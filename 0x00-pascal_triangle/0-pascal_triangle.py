09d96bcf3f8692ee1576d69a9fd3de5ab2242401
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-12-30 15:15:42 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""
-Returns a list of lists of integers representing the Pascal’s triangle of n.
-"""
+"""Pascal Triangle Interview Challenge"""
 
 
 def pascal_triangle(n):
-    """Implements the pascal triangle."""
-    triangle = []
-    if type(n) is not int or n <= 0:
-        return triangle
-    for index in range(n):
-        layer = []
-        for jndex in range(index + 1):
-            if jndex == 0 or jndex == index:
-                layer.append(1)
-            elif index > 0 and jndex > 0:
-                layer.append(triangle[index - 1][jndex - 1] +
-                            triangle[index - 1][jndex])
-        triangle.append(layer)
-    return triangle
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
