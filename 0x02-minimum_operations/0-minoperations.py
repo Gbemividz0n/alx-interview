628e1066283330dacae7ca47304ef40ef510c8e2
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-01-11 06:19:08 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+def minOperations(n):
+    if n <= 0:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        operations += 1
+        if n % i == 0:
+            n = n // i
+            i = 2
+        else:
+            i += 1
+    return operations + 1
