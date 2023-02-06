c1ddba976c81e77ea756662caaaed66bcbc3cd99
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-02-06 03:05:06 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,44 @@
+#!/usr/bin/python3
+""" A script that reads stdin line by line and computes metrics """
+
 import sys
 
-def process_line(line, metrics):
-    parts = line.split()
-    if len(parts) != 9:
-        return
 
-    try:
-        status_code = int(parts[8])
-        file_size = int(parts[-1])
-    except ValueError:
-        return
+def print_status(dict, size):
+    """Print the format"""
+    print("File size: {}".format(size))
+    for key in sorted(dict.keys()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
-    if status_code not in metrics['status_codes']:
-        return
 
-    metrics['total_size'] += file_size
-    metrics['status_codes'][status_code] += 1
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 
-def print_metrics(metrics):
-    print("File size: {}".format(metrics['total_size']))
-    for status_code in sorted(metrics['status_codes'].keys()):
-        print("{}: {}".format(status_code, metrics['status_codes'][status_code]))
+file_size = 0
+count = 0
 
-def main():
-    metrics = {
-        'total_size': 0,
-        'status_codes': {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0
-        }
-    }
-    line_count = 0
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_dict, file_size)
 
-    try:
-        for line in sys.stdin:
-            process_line(line, metrics)
-            line_count += 1
-            if line_count % 10 == 0:
-                print_metrics(metrics)
-    except KeyboardInterrupt:
-        print_metrics(metrics)
+        el = line.split(" ")
+        count += 1
 
-if __name__ == '__main__':
-    main()
+        try:
+            file_size += int(el[-1])
+        except:
+            pass
+
+        try:
+            if el[-2] in status_dict.keys():
+                status_dict[el[-2]] += 1
+        except:
+            pass
+    print_status(status_dict, file_size)
+
+
+except KeyboardInterrupt:
+    print_status(status_dict, file_size)
+    raise
