346481d32dae4e5d777e50685d2d77633367e829
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-03-07 18:47:23 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,44 +12,41 @@ def isWinner(x, nums):
     - Name of the player that won the most rounds.
       If the winner cannot be determined, return None.
     """
-    # Helper function to check if a number is prime
-    def is_prime(n):
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    # Check for invalid input
+    if x < 1 or not nums:
+        return None
 
-    # Function to play a single game
-    def play_game(n):
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        player = 0  # Maria goes first, so player 0 is Maria and player 1 is Ben
-        while primes:
-            p = primes[0]  # Choose the smallest prime
-            if player == 0:
-                player = 1
-            else:
-                player = 0
-            for i in range(p, n + 1, p):
-                if i in primes:
-                    primes.remove(i)
-        return player
+    # Initialize counters for Maria and Ben's wins
+    marias_wins = 0
+    bens_wins = 0
 
-    # Play x rounds of the game and keep track of the winners
-    winners = {"Maria": 0, "Ben": 0}
-    for n in nums:
-        winner = play_game(n)
-        if winner is not None:
-            if winner == 0:
-                winners["Maria"] += 1
-            else:
-                winners["Ben"] += 1
+    # Play the game for each round
+    for i in range(x):
+        n = nums[i]  # Get the value of n for this round
+        primes = [True] * (n+1)  # Initialize a list of primes
+
+        # Use the Sieve of Eratosthenes algorithm to generate primes
+        for p in range(2, int(n**0.5)+1):
+            if primes[p]:
+                for i in range(p*p, n+1, p):
+                    primes[i] = False
+
+        # Count the number of primes in the range [1, n]
+        primes_count = 0
+        for j in range(2, n+1):
+            if primes[j]:
+                primes_count += 1
+
+        # Determine the winner of this round
+        if primes_count % 2 == 1:
+            marias_wins += 1
+        else:
+            bens_wins += 1
 
     # Determine the overall winner
-    if winners["Maria"] > winners["Ben"]:
-        return "Maria"
-    elif winners["Maria"] < winners["Ben"]:
-        return "Ben"
-    else:
+    if marias_wins == bens_wins:
         return None
+    elif marias_wins > bens_wins:
+        return 'Maria'
+    else:
+        return 'Ben'
